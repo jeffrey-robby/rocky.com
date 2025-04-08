a9aed9f090d131dc45070e93ae6a5ade28fddd56
carleii2 <devcarle@gmail.com>
2025-04-08 02:21:48 +0200

    add title to product swiper images

--- html/view/ProductsSwiper.php
+++ html/view/ProductsSwiper.php
@@ -2,11 +2,11 @@
 <div class="swiper-container mySwiper iq-width overflow-hidden mx-auto">
     <div class="swiper-wrapper ">
     <?php
-        $request = mysqli_query($database, "SELECT produits.photo_produits1 FROM produits");
+        $request = mysqli_query($database, "SELECT produits.photo_produits1, produits.nom_produits, produits.description_produits FROM produits");
         while ($result = mysqli_fetch_assoc($request)) {
             echo '
                 <div class="swiper-slide">
-                    <img class="img-fluid w-100 rounded" src="'.$result['photo_produits1'].'" alt="image" />
+                    <img title="'.$result['description_produits'].'" class="img-fluid w-100 rounded" src="'.$result['photo_produits1'].'" alt="image" />
                 </div>
             ';
             # code...
